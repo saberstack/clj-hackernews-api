d11622de27b4c0416e927a10891022a0803d9ca4
Rangel Spasov <raspasov@gmail.com>
2025-07-20 04:32:34 -0700

    clj-hackernews-api: expand API to include user, updates

--- src/org/saberstack/hn/api_v0.clj
+++ src/org/saberstack/hn/api_v0.clj
@@ -1,16 +1,34 @@
 (ns org.saberstack.hn.api-v0
+  "Official HackerNews API docs: https://github.com/HackerNews/API"
   (:require [babashka.http-client :as http-client]))
 
 (def base-uri "https://hacker-news.firebaseio.com/v0/")
 
-(defn uri [suffix id]
-  (str base-uri suffix (str id) ".json"))
+(defn uri
+  ([suffix] (str base-uri suffix ".json"))
+  ([suffix id] (str base-uri suffix "/" id ".json")))
 
-(defn item! [id]
-  (http-client/get (uri "item/" id)))
+(defn item!
+  "Story, comment, etc"
+  [id]
+  (http-client/get (uri "item" id)))
 
-(defn max-item! []
-  (http-client/get (uri "" "maxitem")))
+(defn user!
+  "Retrieves user information by username.
+   Note: username is case-sensitive."
+
+  [username]
+  (http-client/get (uri "user" username)))
+
+(defn max-item!
+  "The current largest item id"
+  []
+  (http-client/get (uri "maxitem")))
+
+(defn updates!
+  "Item and profile changes"
+  []
+  (http-client/get (uri "updates")))
 
 (comment
 
@@ -20,4 +38,6 @@
     item!
     (range 1 11))
 
-  (max-item!))
+  (max-item!)
+
+  (updates!))
