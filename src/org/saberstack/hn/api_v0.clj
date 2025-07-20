4cb453a67a3f3ce00237be632576f021bcce3291
Rangel Spasov <raspasov@gmail.com>
2025-07-20 04:34:35 -0700

    clj-hackernews-api: refactor constants, better URI construction

--- src/org/saberstack/hn/api_v0.clj
+++ src/org/saberstack/hn/api_v0.clj
@@ -2,37 +2,44 @@
   "Official HackerNews API docs: https://github.com/HackerNews/API"
   (:require [babashka.http-client :as http-client]))
 
-(def base-uri "https://hacker-news.firebaseio.com/v0/")
+(def ^:private base-url "https://hacker-news.firebaseio.com/v0/")
+(def ^:private json-suffix ".json")
+(def ^:private endpoints
+  {:item    "item"
+   :user    "user"
+   :maxitem "maxitem"
+   :updates "updates"})
 
-(defn uri
-  ([suffix] (str base-uri suffix ".json"))
-  ([suffix id] (str base-uri suffix "/" id ".json")))
+(defn- uri
+  ([suffix] (str base-url suffix json-suffix))
+  ([suffix id] (str base-url suffix "/" id json-suffix)))
 
 (defn item!
   "Story, comment, etc"
   [id]
-  (http-client/get (uri "item" id)))
+  (http-client/get (uri (:item endpoints) id)))
 
 (defn user!
   "Retrieves user information by username.
    Note: username is case-sensitive."
-
   [username]
-  (http-client/get (uri "user" username)))
+  (http-client/get (uri (:user endpoints) username)))
 
 (defn max-item!
   "The current largest item id"
   []
-  (http-client/get (uri "maxitem")))
+  (http-client/get (uri (:maxitem endpoints))))
 
 (defn updates!
   "Item and profile changes"
   []
-  (http-client/get (uri "updates")))
+  (http-client/get (uri (:updates endpoints))))
 
 (comment
 
   (item! 42)
+
+  (user! "raspasov")
 
   (pmap
     item!
