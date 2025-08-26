91813b2e68216a901c6053e039028e732c3f7854
Rangel Spasov <raspasov@gmail.com>
2025-08-26 03:54:51 -0700

    clj-hackernews-api: stop when items end, sync wip

--- src/org/saberstack/hn/dl.clj
+++ src/org/saberstack/hn/dl.clj
@@ -1,7 +1,10 @@
 (ns org.saberstack.hn.dl
   (:require [charred.api :as charred]
+            [clojure.string :as str]
+            [medley.core :as medley]
             [org.saberstack.hn.api-v0 :as api]
             [taoensso.nippy :as nippy]
+            [babashka.fs :as fs]
             [taoensso.timbre :as timbre]))
 
 (comment
@@ -13,9 +16,17 @@
         api/item!
         (range 1))))
 
-  (nippy/thaw-from-file "items-test-2")
+  (nippy/thaw-from-file "./hndl/items-44642001-44643000")
   )
 
+(defn find-latest-id []
+  (sort-by
+    (fn [s]
+      (parse-long
+        (peek (str/split s #"-"))))
+    (eduction
+      (map str)
+      (fs/list-dir "./hndl"))))
 
 (defonce halt? (atom false))
 (defonce stopped-at (atom nil))
@@ -27,40 +38,42 @@
     {:item-ids    item-ids
      :items       (into [] (map :body) resps)
      :status-200? (every? #(= 200 %) (map :status resps))
-     :resps       resps}))
+     :reached-null? (some? (medley/find-first #(= "null" %) (map :body resps)))
+     :resps resps}))
 
 (defn items-to-disk! [items start-id end-id]
   (nippy/freeze-to-file
     (str "./hndl/" "items-" start-id "-" end-id)
     items))
 
-(defn download-items! [start-id]
+(defn start-sync-items! [start-at-item-id]
   (reset! halt? false)
   (transduce
     (comp
       (partition-all 1000)
       (map pmap-items!)
-      (map (fn [{:keys [items item-ids status-200? resps]}]
+      (map (fn [{:keys [items item-ids status-200? reached-null? resps] :as m}]
              (if status-200?
                (do
                  (items-to-disk! items (first item-ids) (peek item-ids))
                  (reset! last-dl [(first item-ids) (peek item-ids)])
-                 true)
+                 (assoc m :download-ok? true))
                (do
                  (reset! stopped-at resps)
-                 false))))
-      (halt-when (fn [download-ok?]
+                 (assoc m :download-ok? false)))))
+      (halt-when (fn [{:keys [download-ok? reached-null?]}]
                    (or
                      (not download-ok?)
+                     (true? reached-null?)
                      (true? @halt?))))
-      (map (fn [_] (Thread/sleep @sleep-ms))))
+      (map (fn [_] (Thread/sleep ^long @sleep-ms))))
     conj
-    (iterate inc start-id)))
+    (iterate inc start-at-item-id)))
 
 (comment
 
   (reset! halt? true)
   (reset! sleep-ms 20)
   (future
-    (download-items! 25286001))
+    (start-sync-items! 44642001))
   )
